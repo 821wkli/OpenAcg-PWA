812c8b47ea1be43d1d7f81dc32947ba0d4a12659
kwan <kwan@hkinstant.me>
2020-02-18 18:02:31 +0800

    add global prototype

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
-  import Vue from 'vue'
+import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from 'src/router/router'
 import store from 'src/store/'
 import {routerMode} from 'src/config/env'
-import 'src/config/rem'
+import 'src/config/rem';
+import {chinese} from 'src/langs/chinese'
 
-
+Vue.prototype.$lang = {...chinese}
 Vue.use(VueRouter)
 const router = new VueRouter({
-	routes,
-	mode: routerMode,
-	strict: process.env.NODE_ENV !== 'production'
+  routes,
+  mode: routerMode,
+  strict: process.env.NODE_ENV !== 'production'
 })
 
 
 new Vue({
-	router,
-	store,
+  router,
+  store,
 }).$mount('#app')
