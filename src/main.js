7215e6a7262ee25ea604b1bdb5baf0d55af0ec64
LI Wing Kwan <s1185090@studentdmn.ouhk.edu.hk>
2020-03-10 18:14:56 +0800

    remove logs

--- src/main.js
+++ src/main.js
@@ -4,10 +4,9 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import { chinese } from './config/langs'
-import { baseFontSize } from './config/rem'
+import './config/rem'
 import 'vue2-toast/lib/toast.css'
 import Toast from 'vue2-toast'
-console.log(baseFontSize)
 Vue.use(Toast, {
   type: 'center',
   duration: 3000,
